97387aa9cb025fc1ceb9343e672afde301dd3ace
PranayaLovesCoding <86323728+PranayaLovesCoding@users.noreply.github.com>
2021-10-13 19:38:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+    var first_name = "Pranaya";
+
+    var last_name = "Kolla";
+
+    var fullname = first_name.concat(last_name);
+
+    console.log(fullname);
+
+    var x = fullname.length;
+
+    console.log(x);
+
